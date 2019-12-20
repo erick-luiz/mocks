bf82416d58d9df1525b493e4da6659070a36f82f
Erick Luiz <si.erickluiz@gmail.com>
2019-12-20 17:49:16 -0300

    Update TesteController.java

--- src/main/java/com/example/teste/controller/TesteController.java
+++ src/main/java/com/example/teste/controller/TesteController.java
@@ -1,7 +1,8 @@
-package com.example.teste.controller;
+package com.example.demo.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/notification")
 public class TesteController {
-
+	
+	
+	public static Object dados;
+	
 	@PostMapping()
 	public Object createUpdateProduct(@Valid @RequestBody Object request) {
+		dados = request;
 		return request;
 	}
+	
+	@GetMapping()
+	public Object get() {
+		return dados;
+	}
+	
 }
