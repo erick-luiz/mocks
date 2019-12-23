447ec1d757e20ca680964f7560194add13243584
Erick Luiz Fonseca Lopes <902139@lojasrenner.com.br>
2019-12-22 22:39:01 -0300

    insert data

--- src/main/java/com/example/teste/controller/TesteController.java
+++ src/main/java/com/example/teste/controller/TesteController.java
@@ -1,40 +1,57 @@
 package com.example.teste.controller;
 
-import javax.validation.Valid;
+import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.teste.model.Saida;
 
 @RestController
 @RequestMapping("/api/notification")
 public class TesteController {
 	
 	
-	public static Object dados;
+	public static Saida dados = new Saida();
 	
 	@PostMapping()
-	public Object createUpdateProduct(@Valid @RequestBody Object request) {
-		dados = request;
-		return request;
+	public ResponseEntity<?> createUpdateProduct(@RequestBody Object request, @RequestParam Map<String,String> allRequestParams,
+			@RequestHeader Map<String,String> allRequestHeader) {
+		dados.setBody(request);
+		dados.setParams(allRequestParams.toString());
+		dados.setHeaders(allRequestHeader.toString());
+		return ResponseEntity.status(201).body("");
 	}
 	
 	@PostMapping("/{mkt}")
-	public Object createUpdateProduct1(@RequestBody Object request, @PathVariable("mkt") String mkt) {
-		dados = request;
-		return request;
+	public ResponseEntity<?> createUpdateProduct1(@RequestBody Object request, @PathVariable("mkt") String mkt, 
+			@RequestParam Map<String,String> allRequestParams,
+			@RequestHeader Map<String,String> allRequestHeader) {
+		dados.setBody(request);
+		dados.setPath(mkt);
+		dados.setParams(allRequestParams.toString());
+		dados.setHeaders(allRequestHeader.toString());
+		return ResponseEntity.status(201).body("");
 	}
 	
 	@PostMapping("/{mkt}/{sellerId}")
-	public Object createUpdateProduct2(@RequestBody Object request, @PathVariable("mkt") String mkt, @PathVariable("sellerId") String seller) {
-		dados = request;
-		return request;
+	public Object createUpdateProduct2(@RequestBody Object request, @PathVariable("mkt") String mkt, @PathVariable("sellerId") String seller, 
+			@RequestParam Map<String,String> allRequestParams,
+			@RequestHeader Map<String,String> allRequestHeader) {
+		dados.setBody(request);
+		dados.setPath(mkt + "/" + seller); 
+		dados.setParams(allRequestParams.toString());
+		dados.setHeaders(allRequestHeader.toString());
+		return ResponseEntity.status(201).body("");
 	}
-	
-	
+
 	@GetMapping()
 	public Object get() {
 		return dados;
---
+++ src/main/java/com/example/teste/model/Saida.java
@@ -0,0 +1,51 @@
+package com.example.teste.model;
+
+public class Saida {
+
+	private int code = 200;
+	private String params;
+	private String headers;
+	private String path;
+	private Object body;
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getParams() {
+		return params;
+	}
+
+	public void setParams(String params) {
+		this.params = params;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public Object getBody() {
+		return body;
+	}
+
+	public void setBody(Object body) {
+		this.body = body;
+	}
+
+	public String getHeaders() {
+		return headers;
+	}
+
+	public void setHeaders(String headers) {
+		this.headers = headers;
+	}
+
+}
